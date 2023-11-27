2fd22ea55b6c086589d879d1e278063990eb360c
student <s97765@pollub.edu.pl>
2023-11-27 20:00:18 +0100

    LAB 9 zadanie 9.1 poczatek

--- lab6/Contacts.cpp
+++ lab6/Contacts.cpp
@@ -1,70 +1,70 @@
+////
+//// Created by milit on 20.11.2023.
+////
 //
-// Created by milit on 20.11.2023.
+//#include "Contacts.h"
+//#include "map"
 //
-
-#include "Contacts.h"
-#include "map"
-
-Contacts::Contacts() {}
-
-void Contacts::addContact(Contact contact) {
-    if(phone_book.get<0>().count(contact.phone_number)==0) phone_book.insert(contact);
-    else cout<<"Istnieje wpis o danym numerze telefonu!"<<contact.phone_number<<endl;
-}
-void Contacts::deleteContact(int phone_number) {
-    phone_book.get<0>().erase(phone_number);
-}
-
-void Contacts::getAllFromStreet(string street) {
-    cout<<"\nOSOBY MIESZKAJACE NA ULICY "<<street<<" :"<<endl;
-    for(contacts_multi::iterator it=phone_book.begin(); it!=phone_book.end(); it++){
-        if(it->address == street) it->show();
-    }
-}
-
-void Contacts::getFromAgeRange(int range_begin, int range_end) {
-    cout<<"\nOSOBY Z PRZEDZIALU < "<<range_begin<<", "<<range_end<<" > "<<endl;
-    for(contacts_multi::iterator it=phone_book.begin(); it!=phone_book.end(); it++){
-        if( it->age >= range_begin && it->age <= range_end) it->show();
-    }
-}
-
-void Contacts::getFromPhoneNumber(int phone_number) {
-    cout<<"\nOSOBA Z NUMEREM TELEFONU "<<phone_number<<endl;
-    phone_book.get<0>().find(phone_number)->show();
-}
-void Contacts::changeStreet(Contact &c) {
-    c.address = "Sloneczna";
-}
-void Contacts::modifyStreet(string old_street, string new_street) {
-    auto &address_index = phone_book.get<2>();
-    vector<address_type::iterator> vec;
-    for (address_type::iterator it = phone_book.get<2>().begin(); it != phone_book.get<2>().end(); it++) {
-        if(it->address == old_street) vec.push_back(it);
-    }
-    for (int i=0; i<vec.size(); i++) {
-        address_index.modify(vec[i], boost::bind(changeStreet,placeholders::_1));
-    }
-}
-
-void Contacts::countAdults() {
-    int count = 0;
-    for (auto it=phone_book.get<1>().begin(); it!=phone_book.get<1>().end(); it++) if(it->age > 18) count++;
-    cout<<"\nILOSC PELNOLETNICH OSOB: "<<count<<endl;
-
-}
-
-void Contacts::countUniqueSurnames() {
-    map<string, int> surnames;
-    for(const auto & it : phone_book){
-        surnames[it.surname]++;
-    }
-    cout<<"Unikalne nazwiska: ";
-    for (pair<string,int> p : surnames) if(p.second==1) cout<<p.first<<", "<<endl;
-}
-
-void Contacts::showBook() {
-    cout<<"\nKSIAZKA TELEFONICZNA BA BACH:"<<endl;
-    for(phone_type::iterator it=phone_book.get<0>().begin(); it!=phone_book.get<0>().end(); it++) it->show();
-}
-
+//Contacts::Contacts() {}
+//
+//void Contacts::addContact(Contact contact) {
+//    if(phone_book.get<0>().count(contact.phone_number)==0) phone_book.insert(contact);
+//    else cout<<"Istnieje wpis o danym numerze telefonu!"<<contact.phone_number<<endl;
+//}
+//void Contacts::deleteContact(int phone_number) {
+//    phone_book.get<0>().erase(phone_number);
+//}
+//
+//void Contacts::getAllFromStreet(string street) {
+//    cout<<"\nOSOBY MIESZKAJACE NA ULICY "<<street<<" :"<<endl;
+//    for(contacts_multi::iterator it=phone_book.begin(); it!=phone_book.end(); it++){
+//        if(it->address == street) it->show();
+//    }
+//}
+//
+//void Contacts::getFromAgeRange(int range_begin, int range_end) {
+//    cout<<"\nOSOBY Z PRZEDZIALU < "<<range_begin<<", "<<range_end<<" > "<<endl;
+//    for(contacts_multi::iterator it=phone_book.begin(); it!=phone_book.end(); it++){
+//        if( it->age >= range_begin && it->age <= range_end) it->show();
+//    }
+//}
+//
+//void Contacts::getFromPhoneNumber(int phone_number) {
+//    cout<<"\nOSOBA Z NUMEREM TELEFONU "<<phone_number<<endl;
+//    phone_book.get<0>().find(phone_number)->show();
+//}
+//void Contacts::changeStreet(Contact &c) {
+//    c.address = "Sloneczna";
+//}
+//void Contacts::modifyStreet(string old_street, string new_street) {
+//    auto &address_index = phone_book.get<2>();
+//    vector<address_type::iterator> vec;
+//    for (address_type::iterator it = phone_book.get<2>().begin(); it != phone_book.get<2>().end(); it++) {
+//        if(it->address == old_street) vec.push_back(it);
+//    }
+//    for (int i=0; i<vec.size(); i++) {
+//        address_index.modify(vec[i], boost::bind(changeStreet,placeholders::_1));
+//    }
+//}
+//
+//void Contacts::countAdults() {
+//    int count = 0;
+//    for (auto it=phone_book.get<1>().begin(); it!=phone_book.get<1>().end(); it++) if(it->age > 18) count++;
+//    cout<<"\nILOSC PELNOLETNICH OSOB: "<<count<<endl;
+//
+//}
+//
+//void Contacts::countUniqueSurnames() {
+//    map<string, int> surnames;
+//    for(const auto & it : phone_book){
+//        surnames[it.surname]++;
+//    }
+//    cout<<"Unikalne nazwiska: ";
+//    for (pair<string,int> p : surnames) if(p.second==1) cout<<p.first<<", "<<endl;
+//}
+//
+//void Contacts::showBook() {
+//    cout<<"\nKSIAZKA TELEFONICZNA BA BACH:"<<endl;
+//    for(phone_type::iterator it=phone_book.get<0>().begin(); it!=phone_book.get<0>().end(); it++) it->show();
+//}
+//
--- main.cpp
+++ main.cpp
@@ -1,30 +1,120 @@
-//
-// Created by milit on 17.10.2023.
-//
-//
-// Created by student on 16.10.2023.
-//
 
 #include <iostream>
-#include "lab6/Contacts.h"
-#include "lab6/Contact.h"
+#include <iomanip>
+#include <sstream>
+#include <fstream>
 
 using namespace std;
 
-int main(){
-    Contact user1 = {"janek", "jankowski", "Laczna", 42, 997 };
-    Contacts book;
-    book.addContact(user1);
-    book.addContact({"Kamil", "Zdun", "Wesola", 19, 510325123});
-    book.addContact({"Krzysztof", "Zdun", "Wesola", 16, 517325123});
-    book.showBook();
-    book.getAllFromStreet("Wesola");
-    book.modifyStreet("Wesola","Sloneczna");
-    book.showBook();
-    book.countAdults();
-    book.countUniqueSurnames();
-    book.getFromPhoneNumber(997);
-    book.deleteContact(997);
-    book.showBook();
+int main() {
+
+    ifstream fileIf;
+    fileIf.open("code.txt");
+    string line;
+//    while (getline(fileIf,line)) cout<<line<<endl;
+
+    while(!fileIf.eof()){
+        char currentChar = (char)fileIf.get();
+        if (currentChar != '/'){
+            int c2 = fileIf.peek();
+            if ((char)c2 != '/'){
+
+            }
+        }
+//        cout<<"c1: "<<c1<<"  c2: "<<c2<<"  linia: "<<line<<endl;
+
+    }
+    fileIf.close();
+
+//    int a = 223;
+//    float b = 21.365;
+//    cout << "dec: "<<dec<<a<<endl;
+//
+//    cout<<"hex bez showbase: " <<hex<<a<<endl;
+//    cout<<"hex z showbase: " <<showbase<<a<<endl;
+//    cout<<"oct showbase: " <<oct<<a<<endl;
+//    cout<<"oct z showbase: " <<noshowbase<<a<<endl;
+//
+//    cout<<"dec z showpos" <<dec<<showpos<<a<<endl;
+//
+//    cout<<"scientific: " <<scientific<<b<<endl;
+//    cout<<"setw z ritght: " <<setw(20)<<a<<endl;
+//    cout<<"setw z left i setfill: " <<setw(20)<<left<<setfill('*')<<a<<endl;
+//    cout <<"setprecision 3: "<<resetiosflags(ios::scientific)<<setprecision(3)<<b<<endl;
+
+//    string l1,l2,l3="Ala Nowak";
+//    int num;
+//    cout<<"Konwersja: "<<endl;
+//    istringstream isstream("123");
+//    isstream >> num;
+//    cout<<num<<endl;
+//    ostringstream osstream;
+//    osstream << 123;
+//    l1=osstream.str();
+//    cout << l1<< endl;
+//    ostringstream osstream1;
+//    osstream1<< "Laboratorium " << 9 << ", zostalo jeszcze "<<6<<endl;
+//    cout << "Formatowanie: "<<osstream1.str();
+//    string stringEx;
+//    int numberIntEx;
+//    float numberFloatEx;
+//    istringstream isstream1("Ala 10 3.45");
+//    isstream1 >> stringEx >> numberIntEx >> numberFloatEx;
+//    if(isstream1.fail()) {
+//        cout << "Parsowanie nie udalo sie" << endl;
+//        } else {
+//        cout<< "Parsowanie udalo sie:" << endl;
+//        cout<<"Napis: "<< stringEx << endl;
+//        cout<<"Liczba int: "<<numberIntEx<< endl;
+//        cout<<"Liczba float: "<<numberFloatEx <<endl;
+//    }
+//    istringstream ss(l3);
+//    getline(ss, l1, ' ');
+//    getline(ss, l2, ' ');
+//    cout<<l1<<" "<<l2<<endl;
+
+//    ofstream  fileOf;
+//    fileOf.open("Plik.txt");
+//    if(fileOf.is_open()){
+//        cout<<"Plik otworzyl sie poprawnie"<<endl;
+//        fileOf<<"8 Ala ma kota"<<endl;
+//        fileOf<<"a kot ma Ale"<<endl;
+//        fileOf<<10<<endl;
+//        fileOf<<1.1<<endl;
+//        fileOf<<'a'<<endl;
+//        fileOf.close();
+//    } else {
+//        cerr<<"Blad rpzy otwieraniu pliku"<<endl;
+//    }
+
+//    ifstream fileIf;
+//    fileIf.open("Plik.txt");
+//
+//    int number;
+//    fileIf>>number;
+//    string line;
+//    fileIf>>line;
+//    cout<< number << " " << line <<endl;
+//
+//    string line1, line2;
+//    getline(fileIf,line1);
+//    getline(fileIf,line2);
+//
+//    cout<<line1<<endl<<line2<<endl;
+//
+//    char c;
+//    c=fileIf.get();
+//    cout<<c<<endl;
+//
+//    while(!fileIf.eof()) {
+//        getline(fileIf, line1);
+//        cout<<line1<<endl;
+//    }
+//
+//    //alternatywna wersja
+//    while (getline(fileIf,line1)) cout<<line1<<endl;
+//
+//    fileIf.close();
+
     return 0;
 }
